0e2d21689fe4ffcdc3a3f45276812c44293f5063
Andrey <artdron@list.ru>
2020-11-23 11:05:34 +0500

    commit 2

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func HomeRouterHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(w, "Hello, World!")
+	fmt.Fprintf(w, "Hello, World2!")
 }
 func main(){
 	http.HandleFunc("/", HomeRouterHandler)
